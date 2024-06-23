4879e7b78b57c37373243b89e3061a75df0ca382
Fabio Leonardi <fabio.leonardi@ammagamma.com>
2024-06-23 12:24:15 +0200

    database connection refactor

--- backend/src/core/database_connection.py
+++ backend/src/core/database_connection.py
@@ -1,16 +1,78 @@
+# -- Pure Python Imports -- #
+import typing
 # -- Backend Requirements Imports -- #
-from sqlalchemy import create_engine
+from fastapi import Depends
+from sqlalchemy import (
+    create_engine,
+    Engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import (
+    sessionmaker,
+    Session,
+    DeclarativeBase,
+)
 # -- Backend Package Imports -- #
 from src.core import (
-    BackendSettings,
     get_settings,
+    BackendSettings,
 )
 
-backend_settings: BackendSettings = get_settings()
+__all__ = [
+    "get_database_session_from_settings",
+    "get_sqlalchemy_base",
+]
 
-engine = create_engine(backend_settings.get_database_connection_string())
 
-SessionLocal: sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# -- Private Classes & Methods -- #
+
+class DeclarativeBaseSingleton:
+    _instance: typing.Optional[typing.Type[DeclarativeBase]] = None
+
+    @classmethod
+    def get_instance(cls) -> typing.Type[DeclarativeBase]:
+        if cls._instance is None:
+            cls._instance = declarative_base()
+        return cls._instance
+
+
+def _get_engine_from_database_url(
+        database_url: str,
+) -> Engine:
+    return create_engine(
+        url=database_url,
+    )
+
+
+def _get_session_maker_from_engine(
+        engine: Engine,
+) -> sessionmaker:
+    return sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+    )
+
+
+# -- Exposed Methods -- #
+
+def get_database_session_from_settings(
+        backend_settings: BackendSettings = Depends(get_settings),
+) -> typing.Generator:
+    session_maker: sessionmaker = _get_session_maker_from_engine(
+        engine=_get_engine_from_database_url(
+            database_url=backend_settings.get_database_connection_string(),
+        ),
+    )
+    session: Session = session_maker()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+def get_sqlalchemy_base() -> typing.Type[DeclarativeBase]:
+    """
+    Singleton function to get the DeclarativeBase instance for the database.
+    """
+    return DeclarativeBaseSingleton.get_instance()
